6ad8eba53bcbfe0e33b2e5a2561c4feca58c1587
윤시준 <webcodur@gmail.com>
2023-03-17 12:55:14 +0900

    next init;

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "/webcodur.github.io",
+};
+
+module.exports = nextConfig;
+
+// 메인 레포지토리 (아이디.github.io) 라면 /로 적는다
